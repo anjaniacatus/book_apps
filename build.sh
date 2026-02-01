af6e0b60e6545ed9e49e00d10de65157ff67d9ca
afenoum <anja1catus@gmail.com>
2026-02-01 15:24:28 +0300

    user admin on render

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 uv sync
-uv run python manage.py collectstatic --no-input
 uv run python manage.py migrate
+# create superuser without ssh access on render
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+  python manage.py createsuperuser --noinput
+fi
+
+uv run python manage.py collectstatic --no-input
 uv run python manage.py test
