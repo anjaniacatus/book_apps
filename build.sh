07e7de66771cee6f854c4b5a67c7b5ae028a75df
afenoum <anja1catus@gmail.com>
2026-02-01 15:43:28 +0300

    avoid if condition

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 set -o errexit
 uv sync
 uv run python manage.py migrate
-# create superuser without ssh access on render
-if [ "$CREATE_SUPERUSER" = "True" ]; then
-  python manage.py createsuperuser --noinput
-fi
-
+uv run python manage.py createsuperuser --noinput
 uv run python manage.py collectstatic --no-input
 uv run python manage.py test
