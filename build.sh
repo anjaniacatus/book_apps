4689cd0aac0f46bf235399f0757836f0c7663794
afenoum <anja1catus@gmail.com>
2026-02-01 16:17:29 +0300

    .

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 set -o errexit
 uv sync
 uv run python manage.py migrate
-uv run python manage.py createsuperuser --noinput
 uv run python manage.py collectstatic --no-input
 uv run python manage.py test
